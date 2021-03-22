8f1380c2732b5b17d28dacdd82a1ad536a294157
Ben Grissinger <bengrissinger@gmail.com>
2021-03-22 10:24:18 -0400

    Update get_namespaces.sh

--- APIs/get_namespaces.sh
+++ APIs/get_namespaces.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'https://securecloud.tufin.io/lighthouse/<tenant name>/<project name>/namespaces?' -H "Authorization: Bearer <scope all label ci token here>" | jq . 
+curl 'https://<tenant>.securecloud.tufin.io/api/orca/model/clusters/<cluster-name>/namespaces' -H "Authorization: Bearer <scope all label ci token here>" | jq . 
