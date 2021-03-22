724cd768172878831fdab17143a5a34bfc148f15
Ben Grissinger <bengrissinger@gmail.com>
2021-03-22 10:25:43 -0400

    Update get_services.sh

--- APIs/get_services.sh
+++ APIs/get_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'https://<tenant.securecloud.tufin.io/api/orca/model/clusters/<cluster-name>/services' -H "Authorization: Bearer <scope all label ci token here>" | jq .
+curl 'https://<tenant>.securecloud.tufin.io/api/orca/model/clusters/<cluster-name>/services' -H "Authorization: Bearer <scope all label ci token here>" | jq .
