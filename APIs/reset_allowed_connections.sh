988dc64614c765ef30d4087bc0647d3719814969
Ben Grissinger <bengrissinger@gmail.com>
2021-03-22 10:31:44 -0400

    Update reset_allowed_connections.sh

--- APIs/reset_allowed_connections.sh
+++ APIs/reset_allowed_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s -H "Authorization: Bearer "<scope all label ci token here> "https://securecloud.tufin.io/api/<tenant name>/<project name>/scripts/reset-policy")
+bash <(curl -s -H "Authorization: Bearer "<scope all label ci token here> "https://.securecloud.tufin.io/api/orca/model/clusters/<cluster-name>/scripts/reset-policy")
